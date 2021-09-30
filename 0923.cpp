e9708bd3b6dbed8b6b72b3416953e7b9a5f2ab2c
Lty <Lty@LTY-LEO>
2021-09-30 14:51:20 +0200

    添加项目文件。

---
+++ 0923.cpp
@@ -0,0 +1,168 @@
+﻿#include <iostream>
+//#include "0923.h"
+#include <math.h>
+
+using namespace std;
+
+int delta_2nd_eq(float a, float b, float c)
+{
+	if ((b * b - 4 * a * c) >= 1e-6)
+	{
+		return 2;
+	}
+	else if(abs(b * b - 4 * a * c) <= 1e-6)
+	{
+		return 1;
+	}
+	else 
+	{
+		return 0;
+	}
+}
+void solution_2nd_eq(float a, float b, float c)
+{
+	if (abs(a) > 1e-6 || abs(b) > 1e-6 || abs(c) > 1e-6)
+	{
+		cout << "your paras are legal, they are "
+			<< a
+			<< " and "
+			<< b
+			<< " and "
+			<< c
+			<< endl
+			<< endl;
+		if (delta_2nd_eq( a,  b,  c)==2) {
+			cout << "ur delta>0, u have 2 roots, they are >> "
+				<< ((-b + sqrt(b * b - 4 * a * c)) / 2 * a)
+				<< " << and >> "
+				<< ((-b - sqrt(b * b - 4 * a * c)) / 2 * a)
+				<< " <<"
+				<< endl
+				<< endl;
+		}
+		else if (delta_2nd_eq(a, b, c) == 1) {
+			cout << "ur delta=0, u have 1 root, it is >> "
+				<< (-b / (2 * a))
+				<< " <<"
+				<< endl
+				<< endl;
+		}
+		else {
+			cout << "ur delta<0, u have no roots, desole QAQ "
+				<< endl
+				<< endl;
+
+		}
+	}
+	else {
+		cout << " parameter shall not be zero, desole QAQ "
+			<< endl
+			<< endl;
+	}
+}
+void ex10_2nd_eq() {
+	float a = 0;
+	float b = 0;
+	float c = 0;
+	while (1)
+	{
+	cout << "enter your para in order: a b c" 
+		<< endl;
+		if (cin >> a && cin >> b && cin >> c)
+		{
+			solution_2nd_eq(a, b, c);
+		}
+		else 
+		{
+			cout << "ilegal in put" 
+				<< endl
+				<< endl;
+			break;
+		}
+	}
+}
+
+int getN0(float epsl)
+{
+	float u = 0;
+	float  upp = 0;
+	int n = 1;
+	
+	do
+	{
+		u = 1 + (1 / float(n));
+		n++;
+		upp = 1 + (1 / float(n));
+	}
+	while 
+	(abs(upp - u) >= epsl);
+	return (n-1);
+}
+void ex11_series(void) {
+
+	int n = 0;
+	float epsl;
+	while (1) {
+		cout << "input ur sequence difference epsl >> ";
+		if (cin >> epsl)
+		{
+			n=getN0(epsl);
+			
+			cout << "your demande n refers to the epsl is >> "
+				<< n
+				<< " <<"
+				<< endl
+				<< endl;
+		}
+		else
+		{
+			cout << "ilegel input" << endl << endl;
+			break;
+		}
+	}
+}
+
+float sum(int n) 
+{//数列求和，如果变量在分母，必须不能等于零！！！
+	//搞清除从零开始还是从1开始
+	//搞清楚函数的输入变量是什么，输入是什么形参就是什么，输出变量可以用return也可以传参
+	float s = 0;
+		for (int i = 1; i <n; i++)
+		{
+			s +=  (1 / float(i));
+		}
+		return s;
+}
+void ex12_forseries(void) 
+{
+	int n=0;
+	float s = 0;
+	cout << "enter your limit n>> ";
+	if (cin >> n)
+	{
+		s = sum(n);
+		cout << "ur sum is >> " << s << " <<" << endl << endl;
+	}
+	else { cout << "ilegel input!" << endl << endl; }
+}
+
+int main()
+{
+	//try { ex03(); }
+	//catch (const char* msg) {
+	//	cerr << msg << endl;
+	//ex04();
+	//ex05();
+	//ex07();
+	//ex08dowhile01();
+	//ex09while();
+	//ex10secondorderequation;
+	//ex11_series();
+	//ex11_forseries();
+	//ex10_2nd_eq();
+	//ex11_series();
+	//ex12_forseries();
+
+	ex12_forseries();
+
+}
---
+++ 0923.h
@@ -0,0 +1,212 @@
+#include <iostream>
+//constexpr auto pi = 3.14159265358827;
+
+using namespace std;
+//void ex01()
+//{
+//
+//	int n1;
+//	int n2;
+//	cout << "plz enter number n1:";
+//	cin >> n1;
+//	cout << "plz enter number n2:";
+//	cin >> n2;
+//	cout << uppercase
+//		<< "octal \t decimal\t hexadex-digits\n"
+//		<< oct << n1 << "        \t"
+//		<< dec << n1 << "        \t"
+//		<< hex << n1 << endl;
+//	cout << uppercase
+//		<< "octal \t decimal\t hexadex-digits\n"
+//		<< oct << n2 << "        \t"
+//		<< dec << n2 << "        \t"
+//		<< hex << n2 << endl;
+//}
+//void ex02()
+//{
+//	int n1;
+//	int n2;
+//	int s;
+//	std::cout << "plz enter number n1:";
+//	cin >> n1;
+//	cout << "plz enter number n2:";
+//	cin >> n2;
+//	s = n1 + n2;
+//	cout << "n1+n2=" << s << endl;
+//
+//}
+//void ex03()
+//{
+//	
+//	double n1;
+//	double s;
+//	double c;
+//	
+//		cout << "plz enter number n1:";
+//		cin >> n1;
+//		if (!(n1 >= 0 && n1 <= 9)) {
+//			throw"not a number!";
+//		}
+//		else {
+//
+//			s = n1 * n1 * pi;
+//			c = 2 * n1 * pi;
+//			cout << "square=" << s << endl
+//				<< "perimeter=" << c << endl;
+//		}
+//	
+//}
+//void ex04() {
+//	const double KM_per_MILE = 1.609;
+//	double miles,
+//		kms;
+//	cout << "enter how many miles:";
+//	cin >> miles;
+//
+//
+//	if(!(cin >> miles)) { //cin输入错误时执行下边语句
+//		cin.clear(); //清除流标记
+//		cin.sync();  //清空流
+//		cout << "not a number!" << endl; //打印错误提示
+//	}
+//	//if (isdigit(miles)) {
+//	else {
+//		kms = KM_per_MILE * miles;
+//		cout << "the distance in km is " << kms << endl;
+//	}
+////	}
+////	else {
+////cout << "not a number!" << endl;
+////}
+//}
+//void ex05()
+//{
+//	int a = 5;
+//	int b = 9;
+//	float s = 0;
+//	
+//	
+// s = (float)b /(float)a;
+//	
+//	cout << "resualt is:" << s << endl
+//		<< "pi is " << (int)pi << endl;
+//
+//}
+//void ex06() {
+//	auto a = 1;
+//	bool b = 0;
+//	int c = 3;
+//	if (a > b&&c>a) {
+//		cout << "Oh a>b" << endl
+//			;
+//	}
+//	else if(a<b && c>b){ cout << "Oh b<a" << endl; }
+//	else {  }
+//
+//		
+//
+//}
+//void ex07() {
+//	int n =0
+//		,s = 0;
+//	cout << "enter calcule limit:";
+//	cin >> n;
+//
+//	for (int i = 1; i <= n; i++)
+//	{
+//		s += i;
+//	}
+//	cout << "resualt is:" << s;
+//}
+//
+//void ex08dowhile01() {
+//	int i=0, n,s=0;
+//	
+//	cout << "\nenter your limit";
+//	cin >> n;
+//		do
+//		{
+//			s += i;
+//			i++;
+//		} while (i < (n + 1));
+//		cout << "\nresult is >> "<<s<<" <<" << endl;
+//}
+//
+//void ex09while() {
+//	int x, count = 0;
+//	float sum = 0.0;
+//	
+//	cout << "enter num >> ";
+//	while (cin >> x) {
+//		sum += x;
+//		count++;
+//	
+//	}
+//	cout << "average is >> " << sum / float (count) << " <<" << endl;
+//
+//}
+//
+//void ex10secondorderequation() {
+//	float a = 0; 
+//	float b = 0; 
+//	float c = 0;
+//	
+//
+//	while (1) 
+//	{
+//		cout << "enter your para in order: a b c" << endl;
+//	
+//		if (cin >> a && cin >> b && cin >> c)
+//		{
+//			
+//			if (abs(a) > 1e-6 || abs(b) > 1e-6 || abs(c) > 1e-6)
+//			{
+//				cout << "your paras are legal, they are "
+//					<< a
+//					<< " and "
+//					<< b
+//					<< " and "
+//					<< c
+//					<< endl
+//					<< endl;
+//				if (abs(b * b - 4 * a * c) >= 1e-6) {
+//					cout << "ur delta>0, u have 2 roots, they are >> "
+//						<< ((-b + sqrt(b * b - 4 * a * c)) / 2 * a)
+//						<< " << and >> "
+//						<< ((-b - sqrt(b * b - 4 * a * c)) / 2 * a)
+//						<< " <<"
+//						<< endl
+//						<< endl;
+//				}
+//				else if (abs(b * b - 4 * a * c) <= 1e-6) {
+//
+//					cout << "ur delta=0, u have 1 root, it is >> "
+//						<< (-b / (2 * a))
+//						<< " <<"
+//						<< endl
+//						<< endl;
+//				}
+//				else {
+//					cout << "ur delta<0, u have no roots, desole QAQ "
+//						<< endl
+//						<< endl;
+//
+//				}
+//			}
+//			else {
+//				cout << "ur entered zero parameter, desole QAQ " 
+//					<< endl
+//					<< endl;
+//			      }
+//		}
+//		else {
+//			cout << "ilegal in put" << endl
+//				<< endl;
+//			break;
+//		}
+//		
+//
+//
+//	}
+//}
+
